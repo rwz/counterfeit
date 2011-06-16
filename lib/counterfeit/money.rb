2bd85a98955a3e2512c76b0ff27681c6ed0086ce
Pavel Pravosud <rwz@duckroll.ru>
2011-06-16 23:14:09 +0700

    google exchange hack improved, tests added

--- lib/counterfeit/money.rb
+++ lib/counterfeit/money.rb
@@ -1,25 +1,28 @@
 # monkeypatching Money class to catch Money::Bank::UnknownRate error
 
-class Money
-  alias_method :exchange_to_without_google, :exchange_to
-  alias_method :exchange_to, :exchange_to_with_google
+class Money  
+  mattr_accessor :google_saved_the_day
   
-  attr_accessor :google_helped
-  
-  self.google_helped = false
+  self.google_saved_the_day = false
   
   def self.google_to_the_rescue
-    !google_helped && google_helped = true
+    !google_saved_the_day && self.google_saved_the_day = true
   end
   
   def exchange_to_with_google(*args)
     exchange_to_without_google(*args)
   rescue Money::Bank::UnknownRate => e
-    if self.class.google_to_the_rescue
-      Money.default_bank = Money::Bank::GoogleCurrency.new
+    
+    if @bank == Money::Bank::VariableExchange.instance &&
+        self.class.google_to_the_rescue
+        
+      Money.default_bank = @bank = Money::Bank::GoogleCurrency.new
       retry
     else
       raise e
     end
   end
+  
+  alias_method :exchange_to_without_google, :exchange_to
+  alias_method :exchange_to, :exchange_to_with_google
 end
--- spec/counterfeit_spec.rb
+++ spec/counterfeit_spec.rb
@@ -27,4 +27,29 @@ describe Item do
   it 'should work witn has_money alias' do
     Item.new.should respond_to(:another_money_attr)
   end
+  
+  it 'google saved the day' do
+    Money.google_to_the_rescue.should be_true
+    Money.google_to_the_rescue.should be_false
+  end
+  
+  it 'should automatically turn on google exchange on fail' do
+    Money.google_saved_the_day = false
+    Money.default_bank = Money::Bank::VariableExchange.instance
+    money = Money.new(50000, 'USD')
+    lambda do
+      money.exchange_to('EUR')
+    end.should_not raise_error
+    
+    Money.default_bank.should be_kind_of(Money::Bank::GoogleCurrency)
+  end
+  
+  it 'should use the google hack only once' do
+    Money.default_bank = Money::Bank::VariableExchange.instance
+    money = Money.new(50000, 'USD')
+    money.bank.should be_kind_of(Money::Bank::VariableExchange)
+    lambda do
+      money.exchange_to('EUR')
+    end.should raise_error(Money::Bank::UnknownRate)
+  end
 end
